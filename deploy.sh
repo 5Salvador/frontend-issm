41ee6da1630261e1f5f4d6c930c770d5fc5f3549
5Salvador <salvadoramerico155@gmail.com>
2025-11-17 15:23:22 +0200

    Commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+
 echo "Switching to branch Linux"
 git checkout Linux
+
+# Pull the latest changes from the remote 'Linux' branch
+git pull origin Linux
 
 echo "Building app...."
 npm run build
 
+# Check if the build was successful
+if [ $? -ne 0 ]; then
+  echo "Build failed, aborting deployment!"
+  exit 1
+fi
+
 echo "Deploying files to server..."
-scp -r  dist/* issm-p-glpi1@10.0.1.53:/var/www/frontend-issm/
+scp -r dist/* issm-p-glpi1@10.0.1.53:/var/www/frontend-issm/
 
 echo "Done!"
 
