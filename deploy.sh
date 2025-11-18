a7a64d64f0e9d495c52f825451ec05a613b84041
5Salvador <salvadoramerico155@gmail.com>
2025-11-18 08:54:43 +0200

    Teste de Deployemnt

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,20 @@
 echo "Switching to branch Linux"
 git checkout Linux
 
-# Pull the latest changes from the remote 'Linux' branch
 git pull origin Linux
 
 echo "Building app...."
 npm run build
 
-# Check if the build was successful
 if [ $? -ne 0 ]; then
   echo "Build failed, aborting deployment!"
   exit 1
 fi
 
-echo "Deploying files to server..."
+echo "Clearing old files..."
+ssh issm-p-glpi1@10.0.1.53 "sudo rm -rf /var/www/frontend-issm/*"
+
+echo "Deploying files..."
 scp -r dist/* issm-p-glpi1@10.0.1.53:/var/www/frontend-issm/
 
 echo "Done!"
